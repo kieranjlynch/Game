8cb14bf54551235cc45b3a4738f6c6de0968c84b
kieran lynch <k.j.lynch@icloud.com>
2021-07-01 10:57:35 +0100

    extracted the incorrect answers into a new array and pushed the correct answer into it

--- app.js
+++ app.js
@@ -7,12 +7,31 @@ fetch ("https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple")
   console.log(data)
   document.querySelector(".quizQuestion").innerHTML = data.results[0].question;
   document.querySelector("#correct").innerHTML = data.results[0].correct_answer;
-  document.querySelector("#incorrect").innerHTML = data.results[0].incorrect_answers;
+  // document.querySelector("#incorrect").innerHTML = data.results[0].incorrect_answers;
+
+  // const correctAnswer = data.results[0].correct_answer;
+  // const filteredAnswer = correctAnswer.map(correct_answer => "<button>" + correct_answer + "</button>")
+  // const htmlCorrectAnswer = filteredAnswer.join("")
+  // console.log(htmlCorrectAnswer)
+
+  // const returnedAnswers = data.results[0].incorrect_answers;
+  // const filteredAnswers = returnedAnswers.map(incorrect_answers => "<button>" + incorrect_answers + "</button>")
+  // const htmlIncorrectAnswer = filteredAnswers.join("")
+  // console.log(htmlIncorrectAnswer)
+
+const incorrectAnswers = data.results[0].incorrect_answers;
+const incorrectAnswersObj = incorrectAnswers.map(incorrect_answers => {
+  const incorrectObj = { value: incorrect_answers };
+  return incorrectObj;
+});
+incorrectAnswersObj.push({value: data.results[0].correct_answer})
+
+
+
+
 })
 .catch(err => {
   console.log(err)
 })
-
-
 
 
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -6,8 +6,26 @@ fetch("https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple").t
 }).then(function (data) {
   console.log(data);
   document.querySelector(".quizQuestion").innerHTML = data.results[0].question;
-  document.querySelector("#correct").innerHTML = data.results[0].correct_answer;
-  document.querySelector("#incorrect").innerHTML = data.results[0].incorrect_answers;
+  document.querySelector("#correct").innerHTML = data.results[0].correct_answer; // document.querySelector("#incorrect").innerHTML = data.results[0].incorrect_answers;
+  // const correctAnswer = data.results[0].correct_answer;
+  // const filteredAnswer = correctAnswer.map(correct_answer => "<button>" + correct_answer + "</button>")
+  // const htmlCorrectAnswer = filteredAnswer.join("")
+  // console.log(htmlCorrectAnswer)
+  // const returnedAnswers = data.results[0].incorrect_answers;
+  // const filteredAnswers = returnedAnswers.map(incorrect_answers => "<button>" + incorrect_answers + "</button>")
+  // const htmlIncorrectAnswer = filteredAnswers.join("")
+  // console.log(htmlIncorrectAnswer)
+
+  var incorrectAnswers = data.results[0].incorrect_answers;
+  var incorrectAnswersObj = incorrectAnswers.map(function (incorrect_answers) {
+    var incorrectObj = {
+      value: incorrect_answers
+    };
+    return incorrectObj;
+  });
+  incorrectAnswersObj.push({
+    value: data.results[0].correct_answer
+  });
 })["catch"](function (err) {
   console.log(err);
 });
