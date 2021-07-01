126d485f8a1c72938e222e63a43d936cc523f288
kieran lynch <k.j.lynch@icloud.com>
2021-07-01 11:56:21 +0100

    added Fisher-Yates Shuffle to results array

--- app.js
+++ app.js
@@ -7,18 +7,6 @@ fetch ("https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple")
   console.log(data)
   document.querySelector(".quizQuestion").innerHTML = data.results[0].question;
   document.querySelector("#correct").innerHTML = data.results[0].correct_answer;
-  // document.querySelector("#incorrect").innerHTML = data.results[0].incorrect_answers;
-
-  // const correctAnswer = data.results[0].correct_answer;
-  // const filteredAnswer = correctAnswer.map(correct_answer => "<button>" + correct_answer + "</button>")
-  // const htmlCorrectAnswer = filteredAnswer.join("")
-  // console.log(htmlCorrectAnswer)
-
-  // const returnedAnswers = data.results[0].incorrect_answers;
-  // const filteredAnswers = returnedAnswers.map(incorrect_answers => "<button>" + incorrect_answers + "</button>")
-  // const htmlIncorrectAnswer = filteredAnswers.join("")
-  // console.log(htmlIncorrectAnswer)
-
 const incorrectAnswers = data.results[0].incorrect_answers;
 const incorrectAnswersObj = incorrectAnswers.map(incorrect_answers => {
   const incorrectObj = { value: incorrect_answers };
@@ -28,10 +16,17 @@ incorrectAnswersObj.push({value: data.results[0].correct_answer})
 
 
 
+const shuffle = (incorrectAnswersObj) => {
+  let oldElement;
+  for (let i = incorrectAnswersObj.length - 1; i > 0; i--) {
+    let rand = Math.floor(Math.random() * (i + 1));
+    oldElement = incorrectAnswersObj[i];
+    incorrectAnswersObj[i] = incorrectAnswersObj[rand];
+    incorrectAnswersObj[rand] = oldElement;
+  }
+  return incorrectAnswersObj;
+} 
+const result = shuffle(incorrectAnswersObj)
+console.log(result);
 
 })
-.catch(err => {
-  console.log(err)
-})
-
-
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -6,16 +6,7 @@ fetch("https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple").t
 }).then(function (data) {
   console.log(data);
   document.querySelector(".quizQuestion").innerHTML = data.results[0].question;
-  document.querySelector("#correct").innerHTML = data.results[0].correct_answer; // document.querySelector("#incorrect").innerHTML = data.results[0].incorrect_answers;
-  // const correctAnswer = data.results[0].correct_answer;
-  // const filteredAnswer = correctAnswer.map(correct_answer => "<button>" + correct_answer + "</button>")
-  // const htmlCorrectAnswer = filteredAnswer.join("")
-  // console.log(htmlCorrectAnswer)
-  // const returnedAnswers = data.results[0].incorrect_answers;
-  // const filteredAnswers = returnedAnswers.map(incorrect_answers => "<button>" + incorrect_answers + "</button>")
-  // const htmlIncorrectAnswer = filteredAnswers.join("")
-  // console.log(htmlIncorrectAnswer)
-
+  document.querySelector("#correct").innerHTML = data.results[0].correct_answer;
   var incorrectAnswers = data.results[0].incorrect_answers;
   var incorrectAnswersObj = incorrectAnswers.map(function (incorrect_answers) {
     var incorrectObj = {
@@ -26,6 +17,20 @@ fetch("https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple").t
   incorrectAnswersObj.push({
     value: data.results[0].correct_answer
   });
-})["catch"](function (err) {
-  console.log(err);
+
+  var shuffle = function shuffle(incorrectAnswersObj) {
+    var oldElement;
+
+    for (var i = incorrectAnswersObj.length - 1; i > 0; i--) {
+      var rand = Math.floor(Math.random() * (i + 1));
+      oldElement = incorrectAnswersObj[i];
+      incorrectAnswersObj[i] = incorrectAnswersObj[rand];
+      incorrectAnswersObj[rand] = oldElement;
+    }
+
+    return incorrectAnswersObj;
+  };
+
+  var result = shuffle(incorrectAnswersObj);
+  console.log(result);
 });
